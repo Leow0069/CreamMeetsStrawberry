63dfe505d777288a5c5bf04dbb92cef6b3545381
dingyu15 <wdy22@hotmail.com>
2021-12-28 14:35:18 +0800

    Set up React Native Navigation and build the login screen

---
+++ screens/LoginScreen.js
@@ -0,0 +1,33 @@
+import React, { useLayoutEffect } from 'react'
+import { View, Text, Button, ImageBackground, TouchableOpacity } from 'react-native'
+import useAuth from '../hooks/useAuth';
+import { useNavigation } from '@react-navigation/native';
+import tw from 'tailwind-rn';
+
+const LoginScreen = () => {
+    const navigation = useNavigation();
+    //const { user } = useAuth();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false
+        });
+    }, []);
+    
+    return (
+        <View style={tw("flex-1")}>
+            <ImageBackground 
+            resizeMode="cover"
+            style={tw("flex-1")}
+            source={{uri: "https://tinder.com/static/tinder.png"}}>
+            <TouchableOpacity style={[tw("absolute bottom-40 w-52 bg-white p-4 rounded-2xl"), 
+            { marginHorizontal: "25%"},
+            ]} onPress={() => navigation.navigate("Home")}>
+                <Text style={tw("font-bold text-center")}>Sign in & get swiping</Text>
+            </TouchableOpacity>
+            </ImageBackground>
+        </View>
+    )
+}
+
+export default LoginScreen
